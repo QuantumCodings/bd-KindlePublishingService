c1426b66030392398fa07c8dcf2ed38b0718e870
QuantumCodings <166426987+QuantumCodings@users.noreply.github.com>
2025-04-21 14:32:16 -0400

    Update RemoveBookFromCatalogActivity.java

--- src/com/amazon/ata/kindlepublishingservice/activity/RemoveBookFromCatalogActivity.java
+++ src/com/amazon/ata/kindlepublishingservice/activity/RemoveBookFromCatalogActivity.java
@@ -1,5 +1,7 @@
 package com.amazon.ata.kindlepublishingservice.activity;
-
+import com.amazon.ata.kindlepublishingservice.dao.CatalogDao;
+import com.amazon.ata.kindlepublishingservice.dynamodb.models.CatalogItemVersion;
+import com.amazon.ata.kindlepublishingservice.exceptions.BookNotFoundException;
 import com.amazon.ata.kindlepublishingservice.models.requests.RemoveBookFromCatalogRequest;
 import com.amazon.ata.kindlepublishingservice.models.response.RemoveBookFromCatalogResponse;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -7,9 +9,27 @@ import com.amazonaws.services.lambda.runtime.Context;
 import javax.inject.Inject;
 
 public class RemoveBookFromCatalogActivity {
+
+    private final CatalogDao catalogDao;
+
     @Inject
-    RemoveBookFromCatalogActivity() {}
+    public RemoveBookFromCatalogActivity(CatalogDao catalogDao) {
+        this.catalogDao = catalogDao;
+    }
+
     public RemoveBookFromCatalogResponse execute(RemoveBookFromCatalogRequest removeBookFromCatalogRequest) {
-        return null;
+        String bookId = removeBookFromCatalogRequest.getBookId();
+        CatalogItemVersion latestVersion = catalogDao.getBookFromCatalog(bookId);
+        if (latestVersion == null || latestVersion.isInactive()) {
+            throw new BookNotFoundException(String.format("Book with ID %s not found or already inactive.", bookId));
+        }
+        latestVersion.setInactive(true);
+        catalogDao.save(latestVersion);
+
+        return RemoveBookFromCatalogResponse.builder()
+                .withBookId(latestVersion.getBookId())
+                .withVersion(latestVersion.getVersion())
+                .withInactive(true)
+                .build();
     }
 }
